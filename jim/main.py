4591d034c8f46151f12fb51e0eb3d7aaba08c117
Eric <ericv8v9s@outlook.com>
2024-01-21 15:46:23 -0500

    rewrote parser

--- jim/main.py
+++ jim/main.py
@@ -1,30 +1,40 @@
 from jim import reader, interpreter
+from sys import stdin, stdout
 
 
 def main(argv):
 	if len(argv) == 1:
 		repl()
 	elif len(argv) == 2:
-		fname = argv[1]
-		if fname == "-":
-			fname = "/dev/stdin"
-		with open(fname) as f:
-			for form in reader.feed(f.read()):
-				interpreter.evaluate(form)
+		run_file(argv[1])
 	else:
 		print(f"Usage: {argv[0]} [file]")
 
 
 def repl():
+	read_char = lambda: stdin.read(1)
 	while True:
-		try:
-			line = input("IN: ")
-		except EOFError:
-			return
+		print("IN: ", end="")
+		stdout.flush()
+		parsed = reader.parse(read_char)
+		if parsed is None:
+			print()
+			break
+		print("OUT:", interpreter.evaluate(parsed))
+		stdout.flush()
 
-		for form in reader.feed(line):
-			result = interpreter.evaluate(form)
-			print("OUT: ", result)
+
+def run_file(fname):
+	if fname == "-":
+		f = stdin
+	else:
+		f = open(fname)
+	with f:
+		while True:
+			form = reader.parse(lambda: f.read(1))
+			print(interpreter.evaluate(form))
+			if form is None:
+				break
 
 
 if __name__ == "__main__":
