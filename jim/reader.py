740ad32a74a9f55a840c70a300c5d016baff4b50
Eric <ericv8v9s@outlook.com>
2024-02-02 21:55:56 -0500

    fixed reader bug
    Reader used to ignore the first character following parse() calls.
    Added a buffer to properly support character put back.

--- jim/reader.py
+++ jim/reader.py
@@ -1,8 +1,24 @@
 """
 The reader: string in, forms out.
+
+Character source discipline:
+The parse function takes input from a character source (get_next_char).
+This means characters are consumed one at a time,
+and that once consumed they cannot be put back.
+A buffer is used to remedy this.
+
+Each of the specialized parse functions (parse_form, parse_str, etc.)
+expect the next call to get_next_char to produce the very first character
+within the relevant object.
+For parse_form, for example, the first call of get_next_char must return a '('.
+Consequently, upon completion of a parse, the function is responsible for
+putting back any character(s) consumed to determine termination,
+but are not part of the object being parsed.
 """
 
 from string import whitespace, digits, ascii_letters
+from collections import deque
+
 _SPACES = set(whitespace)
 _DIGITS = set(digits)
 _LETTERS = set(ascii_letters)
@@ -12,7 +28,22 @@ _IDENT_FIRST_CHARS = _LETTERS | _PUNCTS
 _IDENT_CHARS = _IDENT_FIRST_CHARS | _DIGITS
 
 
+_buffer = deque()
+
+def _wrap_char_source(get_next_char):
+	def wrapper():
+		if len(_buffer) > 0:
+			return _buffer.popleft()
+		return get_next_char()
+	return wrapper
+
+def _put_back(c):
+	_buffer.appendleft(c)
+
+
 def parse(get_next_char):
+	get_next_char = _wrap_char_source(get_next_char)
+
 	c = get_next_char()
 	while True:
 		if c in _SPACES:
@@ -36,16 +67,19 @@ def parse(get_next_char):
 	else:
 		raise ValueError(f"illegal character: '{c}'")
 
-	return pf(c, get_next_char)[1]
+	_put_back(c)
+
+	return pf(get_next_char)
 
 
-def parse_form(current_char, get_next_char) -> tuple[str, list]:
-	assert current_char == '('
+def parse_form(get_next_char) -> list:
 	c = get_next_char()
+	assert c == '('
+
 	elements = []
 	while True:
+		c = get_next_char()
 		if c in _SPACES:
-			c = get_next_char()
 			continue
 		if c == '(':
 			pf = parse_form
@@ -56,18 +90,21 @@ def parse_form(current_char, get_next_char) -> tuple[str, list]:
 		elif c in _IDENT_FIRST_CHARS:
 			pf = parse_symbol
 		elif c == ')':
-			return (get_next_char(), elements)
+			return elements
 		elif c == "":
 			raise EOFError
 		else:
 			raise ValueError(f"illegal character: '{c}'")
-		c, ele = pf(c, get_next_char)
-		elements.append(ele)
+
+		_put_back(c)
+		elements.append(pf(get_next_char))
+
 	assert False
 
 
-def parse_str(current_char, get_next_char) -> tuple[str, tuple[str, str]]:
-	assert current_char == '"'
+def parse_str(get_next_char) -> tuple[str, str]:
+	c = get_next_char()
+	assert c == '"'
 	s = ""
 	while True:
 		c = get_next_char()
@@ -75,7 +112,7 @@ def parse_str(current_char, get_next_char) -> tuple[str, tuple[str, str]]:
 			s += get_next_char()
 			c = get_next_char()
 		elif c == '"':
-			return (get_next_char(), ("LIT", s))
+			return ("LIT", s)
 		elif c == "":
 			raise EOFError
 		else:
@@ -83,12 +120,13 @@ def parse_str(current_char, get_next_char) -> tuple[str, tuple[str, str]]:
 	assert False
 
 
-def parse_int(current_char, get_next_char) -> tuple[str, tuple[str, int]]:
-	s = current_char
+def parse_int(get_next_char) -> tuple[str, int]:
+	s = get_next_char()
 	while True:
 		c = get_next_char()
 		if c in _SPACES or c == '(' or c == ')' or c == "":
-			return (c, ("LIT", int(s)))
+			_put_back(c)
+			return ("LIT", int(s))
 		elif c in _DIGITS:
 			s += c
 		else:
@@ -96,12 +134,13 @@ def parse_int(current_char, get_next_char) -> tuple[str, tuple[str, int]]:
 	assert False
 
 
-def parse_symbol(current_char, get_next_char) -> tuple[str, tuple[str, str]]:
-	s = current_char
+def parse_symbol(get_next_char) -> tuple[str, str]:
+	s = get_next_char()
 	while True:
 		c = get_next_char()
 		if c in _IDENT_CHARS:
 			s += c
 		else:
-			return (c, ("SYM", s))
+			_put_back(c)
+			return ("SYM", s)
 	assert False
