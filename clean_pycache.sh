c05ece055f32e1cd22e6b034368b6dfcb1478bba
Eric <ericv8v9s@outlook.com>
2025-03-30 04:16:21 -0400

    changed clean_pycache to only clean jim dir

--- clean_pycache.sh
+++ clean_pycache.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-find . -type d -name __pycache__ -exec echo rm -r {} +
-find . -type d -name __pycache__ -exec rm -r {} +
+find ./jim -type d -name __pycache__ -exec echo rm -r {} +
+find ./jim -type d -name __pycache__ -exec rm -r {} +
